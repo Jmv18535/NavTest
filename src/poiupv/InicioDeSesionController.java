adbbd60a1856d1c5e08727da0a0f85d90489605b
jasan <jasan@LAPTOP-RSOJNSEB>
2022-05-07 13:08:14 +0200

    inicio de sesión botón cancelar

--- src/poiupv/InicioDeSesionController.java
+++ src/poiupv/InicioDeSesionController.java
@@ -5,9 +5,18 @@
  */
 package poiupv;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,6 +25,10 @@ import javafx.fxml.Initializable;
  */
 public class InicioDeSesionController implements Initializable {
 
+    @FXML
+    private TextField texto_usuario;
+    @FXML
+    private TextField texto_password;
     /**
      * Initializes the controller class.
      */
@@ -23,5 +36,21 @@ public class InicioDeSesionController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void PulsadoCancelar(ActionEvent event) throws IOException {
+        Parent inicioSesionParent = FXMLLoader.load(getClass().getResource("PantallaInicial.fxml"));
+         
+        Scene inicioDeSesion = new Scene(inicioSesionParent);
+        
+        Stage ventana= (Stage)((Node)event.getSource()).getScene().getWindow();
+        ventana.setScene(inicioDeSesion);
+        ventana.setResizable(true);
+        ventana.show();
+    }
+
+    @FXML
+    private void PulsadoEntrar(ActionEvent event) {
+    }
     
 }
